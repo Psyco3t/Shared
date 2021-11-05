cb2032441f000473130486359413efd0e2bb4031
Psyco3t <59059968+Psyco3t@users.noreply.github.com>
2021-11-05 12:12:46 +0100

    1.feladat

---
+++ fel1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.fel
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<int> nums = new List<int>(); //list for data
+      Console.WriteLine("Kerem a szamok szamat: ");
+      string db = Console.ReadLine();
+      int db2 = int.Parse(db);
+      Random rand = new Random();
+      int randy=rand.Next(10, 31);
+      nums.Add(randy);
+      for (int i=0;i < db2; i++)
+      {
+        int other = rand.Next(1, 6) + randy;
+        randy = other;
+        nums.Add(randy);
+      }
+      foreach (var item in nums)
+      {
+        Console.WriteLine(item);
+      }
+    }
+  }
+}
